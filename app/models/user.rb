9f22298197bc067413dddab8bd89ad967273a1d8
Gabriel Ginn Suarez <ginnlaiho@gmail.com>
2020-11-11 20:07:41 -0600

    bugfixes: fix a bug that was displaying the error message twice

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
   validates :username, :email, :password, presence: true
   validates :username, uniqueness: true
-  validates :email, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i, on: :create }
+  validates :email, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i }
   validates :password, confirmation: true
 end
